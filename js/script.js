0c634d8337e4c3868927cabbfacf5555eca2c65d
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-06 18:37:26 -0500

    add display books feature

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let n = 0;
 
 
 function Book(form) {
@@ -13,6 +14,32 @@ function addBookToLibrary(form) {
 	event.preventDefault();
 	let book = new Book(form);
 	myLibrary.push(book);
+	dispaly()
 	form.reset();
 
+}
+
+function dispaly(){
+	const Lib = document.getElementById("lib-container");
+	n++;
+	for( let i = n-1; i < myLibrary.length; i++ ) {
+		let newbook = document.createElement('div');
+		let titlediv = document.createElement('div');
+		let authordiv = document.createElement('div');
+		let pagesdiv = document.createElement('div');
+		let readdiv = document.createElement('div');
+
+		titlediv.textContent = myLibrary[i].title;
+		authordiv.textContent = myLibrary[i].author;
+		pagesdiv.textContent = myLibrary[i].pages;
+		readdiv.textContent = myLibrary[i].read;
+
+		Lib.appendChild(newbook);
+		newbook.appendChild(titlediv);
+		newbook.appendChild(authordiv);
+		newbook.appendChild(pagesdiv);
+		newbook.appendChild(readdiv);
+
+		
+	}
 }
